1495e0f3572544d347c75652bd819d2baa7a270a
Matt Vogel <mainmoniker@googlemail.com>
2023-07-28 23:08:34 -0400

    feat: test slash command

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, DropdownComponent } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -13,73 +13,115 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'import-book-info',
+			name: 'Import Book Info',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+				let activeFile = this.app.workspace.getActiveFile();
+				
+				if (activeFile && activeFile.path.startsWith('Book/')) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						// this.importBookInfo(activeFile);
+						this.bookModal(activeFile);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
+				} else {
+					return false;
 				}
 			}
+
 		});
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
+	}
+	async bookModal(file: TFile) {
+        let options = {
+            'option1': 'Option 1',
+            'option2': 'Option 2',
+            // ...
+        };
+        let modal = new BookInfoModal(this.app, file, options);
+        modal.open();
+    }
+	
+	async importBookInfo(file: TFile) {
+		const fname = this.app.workspace.getActiveFile()?.name.replace(".md","")
+		const url = "https://openlibrary.org/search.json?title=" + fname + "";
+		console.log(url)
+		let activeFile = this.app.workspace.getActiveFile();
+		if (activeFile) {
+			let fileContent = await this.app.vault.read(activeFile);
+			console.log(fileContent)
+			let newContent = this.addOrUpdateMetadata(fileContent);
+			console.log(newContent)
+			// await this.app.vault.modify(activeFile, newContent);
+		}
+	}
+	
+	addOrUpdateMetadata(content: string): string {
+		const results = [
+			{value:{
+				cover:"COVER_URL",
+				title:"Thinking Fast and Slow",
+				authors:"Daniel Kahneman",
+				firstYear:"YEAR_PUBLISHED",
+				pages:100
+			}},
+			{value:{
+				subjects:["BOOK_SUBJECT"],
+				description:"BOOK_SUMMARY"
+			}}
+		];
+	
+		let metadata = {};
+		let body = content;
+	
+		// Check if the file already has YAML front matter
+		if (content.startsWith('---\n')) {
+			let endOfFrontMatter = content.indexOf('\n---', 4);
+			if (endOfFrontMatter !== -1) {
+				let frontMatter = content.substring(4, endOfFrontMatter);
+				body = content.substring(endOfFrontMatter + 4);
+	
+				// Parse the existing front matter
+				frontMatter.split('\n').forEach(line => {
+					let colonIndex = line.indexOf(':');
+					if (colonIndex !== -1) {
+						let key = line.substring(0, colonIndex).trim();
+						let value = line.substring(colonIndex + 1).trim();
+						metadata[key] = value;
+					}
+				});
+			}
+		}
+	
+		// Add the new metadata only if the key does not already exist
+		for (let result of results) {
+			for (let key in result.value) {
+				if (!metadata.hasOwnProperty(key)) {
+					metadata[key] = result.value[key];
+				}
+			}
+		}
+	
+		// Convert the metadata back to YAML front matter format
+		let frontMatter = '---\n';
+		for (let key in metadata) {
+			frontMatter += `${key}: ${metadata[key]}\n`;
+		}
+		frontMatter += '---\n';
+	
+		return frontMatter + body;
 	}
 
 	async loadSettings() {
@@ -91,20 +133,48 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class BookInfoModal extends Modal {
+    file: TFile;
+    options: Record<string, string>;
+
+    constructor(app, file, options) {
+        super(app);
+        this.file = file;
+        this.options = options;
+    }
+
+    onOpen() {
+		let {contentEl} = this;
+	
+		// Create a header
+		contentEl.createEl('h2', { text: 'Open Book Library' });
+	
+		// Create a label for the dropdown
+		contentEl.createEl('p', { text: 'Which do you mean?' });
+	
+		// Create a div for the dropdown
+		let dropdownDiv = contentEl.createDiv();
+		let dropdown = new DropdownComponent(dropdownDiv);
+	
+		// Add options to the dropdown
+		for (let value in this.options) {
+			dropdown.addOption(value, this.options[value]);
+		}
+	
+		// Create a div for the submit button
+		let buttonDiv = contentEl.createDiv({ attr: { style: 'text-align: right;' } });
+		let submitButton = buttonDiv.createEl('button', { text: 'Submit' });
+		submitButton.onClickEvent(() => {
+			let selectedOption = dropdown.getValue();
+			this.submit(selectedOption);
+		});
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    submit(selectedOption) {
+        // Here you can run more functions with the selected item as an input
+        console.log('Selected option:', selectedOption);
+        this.close();
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
